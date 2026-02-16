a3d0306e163818f239da530ce0a8b9d8a28bbaf7
aptsudokpy <66010914154@msu.ac.th>
2026-02-16 20:59:50 +0700

    hee

--- admin/logout.php
+++ admin/logout.php
@@ -3,7 +3,7 @@ session_start();
 session_unset();
 session_destroy(); // ล้างข้อมูล Session ทั้งหมด
 
-// สั่งให้เด้งไปหน้า Login ของ Admin
-header("Location: login_admin.php");
+// สั่งให้เด้งไปหน้า Login ของ User (ระบุ path ให้ถูกต้อง)
+header("Location: user_interface/login.php");
 exit();
 ?>
